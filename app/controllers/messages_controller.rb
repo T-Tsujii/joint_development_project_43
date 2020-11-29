2ab7c263efd655a3b05305d1c0f9d3146603cccb
OhtakeYusuke <o.yusuke0501@gmail.com>
2020-11-29 20:46:46 +0900

    新規登録追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to messages_url
+    else
+      render :new
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :content)
+  end
 end
