c05772d78867455b354af56ed9cb9ec7a25165c3
OhtakeYusuke <o.yusuke0501@gmail.com>
2020-11-29 20:20:57 +0900

    メッセージモデル、マイグレーション ファイル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_152211) do
+ActiveRecord::Schema.define(version: 2020_11_29_111703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
