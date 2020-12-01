37299467ea489400d9ea17f6a4f0357c12eff92d
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2020-12-02 00:53:54 +0900

    一覧表示機能を実装　（タイトルのみを表示）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,19 @@
 class TasksController < ApplicationController
 
+    def index
+        @tasks = Task.select(:title)
+    end
     def new
         @task = Task.new
     end
 
     def create
         Task.create(task_params)
+        redirect_to tasks_path
     end
 
     private
     def task_params
-        params(:task).permit(:title, :content)
+        params.require(:task).permit(:title, :content)
     end
 end
